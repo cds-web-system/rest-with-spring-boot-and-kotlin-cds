13067140220bde5a7d3850158143fa8738f59e3d
CDS WebSystem <cdswebsystem@gmail.com>
2022-05-19 14:18:32 -0300

    Projeto inicial

---
+++ rest-with-spring-boot-and-kotlin-cds/src/main/kotlin/br/com/cds/RestWithSpringBootAndKotlinCdsApplication.kt
@@ -0,0 +1,11 @@
+package br.com.cds
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestWithSpringBootAndKotlinCdsApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestWithSpringBootAndKotlinCdsApplication>(*args)
+}
---
+++ rest-with-spring-boot-and-kotlin-cds/src/test/kotlin/br/com/cds/RestWithSpringBootAndKotlinCdsApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.cds
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestWithSpringBootAndKotlinCdsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
